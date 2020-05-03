a773680c008ab7de31beb2c394b4b8b02c32de7f
punkkk <punkrus18@gmail.com>
2020-05-03 11:15:43 +0300

    add: cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 import {greetingsRouter} from "./routes";
 
 const app = new Koa();
 
+app.use(cors());
 app.use(bodyParser());
 
 app.use(greetingsRouter.routes());
